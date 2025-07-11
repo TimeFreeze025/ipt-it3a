eb86243aa67d070d41b360e3ade7106c29ebef81
Dave Emmanuel Nuada <davenuada025@gmail.com>
2025-07-11 13:58:18 +0800

    First Commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Page2() {
+  return (
+    <main>
+      <h1>PAGE 2</h1>
+      <h2>This is page 2</h2>
+    </main>
+  );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import "~/styles/globals.css";
+
+import { type Metadata } from "next";
+import { Geist } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "Demo IPT IT3A",
+  description: "Demo application for IPT IT3A course",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${geist.variable}`}>
+      <body>
+        <main>
+          <h1 className="text-white">Hello World</h1>
+        </main>
+        {children}
+      </body>
+    </html>
+  );
+}
